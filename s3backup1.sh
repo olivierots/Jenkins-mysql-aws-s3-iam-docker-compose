58a8d3f17603526bb00f3361eb3fb32407df4413
Olivier Otshudi <olivierots@hotmail.com>
2020-06-16 22:47:51 +0100

    Update s3backup1.sh

--- s3backup1.sh
+++ s3backup1.sh
@@ -9,7 +9,7 @@ AWS_SECRET_KEY=$4
 BUCKET_NAME=$5
 
 mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
-export AWS_ACCESS_KEY_ID=AKIAUQQVQNBJ2ZPEDURY
+export AWS_ACCESS_KEY_ID=XXX
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY && \
 echo "Uploading $BACKUP to s3 now" && \
 aws s3 cp /tmp/db-$DATE.sqlbackup s3://$BUCKET_NAME/$BACKUP
